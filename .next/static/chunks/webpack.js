e65049b6637af92d4cf0dec1fbafa7cce5230de8
emmanuelprince66 <emmanuelochigbo60@gmail.com>
2022-09-14 10:57:16 +1300

    first commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3875c998cf50729b"; }
+/******/ 		__webpack_require__.h = function() { return "0bd690af24fb1134"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
